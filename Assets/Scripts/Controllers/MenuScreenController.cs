aa347deadb1eb15de02e7bc462cba79efc2176bd
jKarremans <jerzykarremans@hotmail.com>
2025-10-03 14:12:44 +0200

    updated menu screen controller

---
+++ Assets/Scripts/Controllers/MenuScreenController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
+
+public class MenuScreenController : MonoBehaviour
+{
+
+    void Start()
+    {
+        Button[] buttons = GetComponentsInChildren<Button>();
+
+        buttons[0].onClick.AddListener(() =>
+        {
+            GameSettingsData.enemyType = EnemyType.SingleScreen;
+            LoadBoardScreen();
+        });
+        buttons[1].onClick.AddListener(() =>
+        {
+            GameSettingsData.enemyType = EnemyType.Bot;
+            LoadBoardScreen();
+        });
+        buttons[2].onClick.AddListener(() =>
+        {
+            GameSettingsData.enemyType = EnemyType.Multiplayer;
+            LoadBoardScreen();
+        });
+    }
+
+    private void LoadBoardScreen()
+    {
+        SceneManager.LoadScene("BoardScreen");
+    }
+}
